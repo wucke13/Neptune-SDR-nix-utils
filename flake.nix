97b781b3d7a1fcb8116ae6348d3443948049edfd
wucke13 <wucke13+github@gmail.com>
2024-12-29 02:57:18 +0100

    chore: re-fmt flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,49 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ]
-      (system:
-        let
-          # packages for the current host system
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default ];
-          };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        # packages for the current host system
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
 
-          # packages for the SDR processor
-          pkgsCross = import nixpkgs {
-            inherit system;
-            crossSystem = {
-              config = "armv7l-unknown-linux-gnueabihf";
-              isStatic = true;
-            };
+        # packages for the SDR processor
+        pkgsCross = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+          crossSystem = {
+            config = "armv7l-unknown-linux-gnueabihf";
           };
-        in
-        {
-          packages = { inherit (pkgs) iio-oscilloscope soapyplutosdr urh; };
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.gnuradio
-              pkgs.libiio
-              pkgs.iio-oscilloscope
-              # pkgs.sdrangel
-              pkgs.sigdigger
-              pkgs.soapysdr
-              pkgs.urh
-              pkgs.gqrx
-            ];
-          };
-        }
-      ) // {
+        };
+      in
+      {
+        packages = {
+          inherit (pkgs) iio-oscilloscope soapyplutosdr;
+        };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            (pkgs.soapysdr.override { extraPackages = [ self.packages.${system}.soapyplutosdr ]; })
+            pkgs.gnuradio
+            pkgs.gqrx
+            pkgs.iio-oscilloscope
+            pkgs.libiio
+            # pkgs.sdrangel
+            pkgs.sigdigger
+            pkgs.urh
+          ];
+        };
+      }
+    )
+    // {
       overlays.default = import ./overlay.nix;
     };
 }
